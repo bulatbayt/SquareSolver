8561f441400f2a2b4b872bdf973d49f4e2fb52e9
BulatBaytMIPT <bulatbayt@gmail.com>
2026-08-28 18:35:23 +0300

    add mode.h

---
+++ mode.h
@@ -0,0 +1,9 @@
+void mode_choise (double* x1, double* x2, struct coeff* square, char* mode, FILE* note);
+
+void coefficient_mode (double* x1, double* x2, struct coeff* square, char* mode, FILE* note);
+
+void complete_mode (double* x1, double* x2, struct coeff* square, char* mode, FILE* note);
+
+void test_mode (char* mode, FILE* note);
+
+void help_mode (char* mode);

23d0cc1e65ac5ab39efc55245f98a4c17f6d984a
BulatBaytMIPT <bulatbayt@gmail.com>
2026-08-28 18:30:29 +0300

    add new function prototype

--- solver.h
+++ solver.h
@@ -1,4 +1,4 @@
-enum desion { NO_DECISION, ONE_DECISION, TWO_DECISION, ENDLESSLY_DECISION};
+enum decision { NO_DECISION, ONE_DECISION, TWO_DECISION, ENDLESSLY_DECISION};
 
 struct coeff {
     double a;
@@ -6,6 +6,14 @@ struct coeff {
     double c;
 };
 
-int info_processing (struct coeff values, double* x1, double* x2);
+int solve_square (struct coeff* values, double* x1, double* x2);
 
 int is_zero (double value);
+
+double my_pow (double square, int degree);
+
+//double rate_discriminant (struct coeff* square);
+
+char* corrected_string (char* string_squarte);
+
+void theorem_Vieta (double x1, double x2, struct coeff* square);

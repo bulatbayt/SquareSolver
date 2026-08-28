fcbe839f67e542a98c56b33e4a62a931dc0264e0
BulatBaytMIPT <bulatbayt@gmail.com>
2026-08-28 18:34:37 +0300

    Create test.h

---
+++ test.h
@@ -0,0 +1,7 @@
+int RunTests_txt (int amount, FILE* note);
+
+void RunOneTest_rand (struct coeff* square);
+
+void RunTests_rand (int amount);
+
+void RunOneTest_txt (struct coeff* square, int nRootsRef, double x1Ref, double x2Ref);
